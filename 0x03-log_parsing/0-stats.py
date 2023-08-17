b1dd4bde51f7c59c054738aa01d6d1a2a4f67cc8
omah-hub <orjichioma422@yahoo.com>
2023-08-17 12:57:30 +0000

    my commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,37 @@
 #!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics:
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
-                code> <file size>
-"""
+'''Write script that reads stdin line by line and then computes metrics'''
+
 
 import sys
-code = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
-sum = 0
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print_stats():
-    """
-    Function that print stats about log
-    """
-    global sum
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    print('File size: {}'.format(sum))
-    stcdor = sorted(code.keys())
-    for each in stcdor:
-        if code[each] > 0:
-            print('{}: {}'.format(each, code[each]))
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
+except Exception as err:
+    pass
 
-if __name__ == "__main__":
-    cnt = 0
-    try:
-        """ Iterate the standard input """
-        for data in sys.stdin:
-            try:
-                fact = data.split(' ')
-                """ If there is a status code """
-                if fact[-2] in code:
-                    code[fact[-2]] += 1
-                """ If there is a length """
-                sum += int(fact[-1])
-            except Exception:
-                pass
-            """ Printing control """
-            cnt += 1
-            if cnt == 10:
-                print_stats()
-                cnt = 0
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    else:
-        print_stats()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
